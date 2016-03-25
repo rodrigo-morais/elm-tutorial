e3d80723f661baa8644c80f4eb7e6bb239a08348
Rodrigo Morais <morais.rm@gmail.com>
2016-03-25 18:43:04 -0300

    Example to Tasks.

---
+++ tasks/start-app.elm
@@ -0,0 +1,86 @@
+module Main (..) where
+
+import Effects exposing (Effects, Never)
+import Html
+import Html.Events as Events
+import Http
+import StartApp
+import Task
+
+
+type Action
+  = NoOp
+  | Refresh
+  | OnRefresh (Result Http.Error String)
+
+
+type alias Model =
+  String
+
+
+view : Signal.Address Action -> Model -> Html.Html
+view address message =
+  Html.div
+    []
+    [ Html.button
+        [ Events.onClick address Refresh 
+        ]
+        [ Html.text "Refresh" 
+        ]
+    , Html.text message
+    ]
+
+
+httpTask : Task.Task Http.Error String
+httpTask =
+  Http.getString "http://localhost:3000/"
+
+
+refreshFx : Effects.Effects Action
+refreshFx =
+  httpTask
+    |> Task.toResult
+    |> Task.map OnRefresh
+    |> Effects.task
+
+
+init : ( Model, Effects Action )
+init =
+  ( "", refreshFx )
+
+
+update : Action -> Model -> ( Model, Effects.Effects Action )
+update action model =
+  case Debug.log "action" action of
+    Refresh ->
+      ( model, refreshFx )
+
+    OnRefresh result ->
+      let
+        message =
+          Result.withDefault "" result
+      in
+        ( message, Effects.none )
+
+    _ ->
+      ( model, Effects.none )
+
+
+app : StartApp.App Model
+app =
+  StartApp.start
+    { init = init
+    , inputs = []
+    , update = update
+    , view = view
+    }
+
+
+main : Signal.Signal Html.Html
+main =
+  app.html
+
+
+port runner : Signal (Task.Task Never ())
+port runner =
+  app.tasks
